61a77426851c34e74a01a4df88df733c1fd010f8
Richard Russell <9446579+RussellAjax@users.noreply.github.com>
2020-07-06 18:20:58 +0700

    Refix database config

--- db/server.js
+++ db/server.js
@@ -1,11 +1,9 @@
-const dbConfig = require('./config.js');
-
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
 const db = {};
 db.mongoose = mongoose;
-db.url = dbConfig.url
+db.url = 'mongodb://localhost:27017/todo-db';
 db.models = require('./model.js');
 
 module.exports = db;
